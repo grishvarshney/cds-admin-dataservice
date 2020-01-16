4612d3aebb808d0e8676cac55b60b987f0087a72
Vikas Garg <vikasgarg333@gmail.com>
2020-01-16 10:26:42 -0500

    serach functinality

--- src/main/java/com/cdsadmin/dataservice/repository/MergerRepository.java
+++ src/main/java/com/cdsadmin/dataservice/repository/MergerRepository.java
@@ -4,6 +4,9 @@ import com.cdsadmin.dataservice.domain.Merger;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 /**
  * Spring Data  repository for the Merger entity.
@@ -11,5 +14,5 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface MergerRepository extends JpaRepository<Merger, Long> {
-
+    List<Merger> findByCustomerFromOrCustomerTo(Long customerFrom, Long customerTo);
 }
